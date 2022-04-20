b1c15e5b2ee3a2046c1228ac53b838f96fb1392d
omimanav <omimanav@icloud.com>
2022-04-20 12:17:43 +1200

    added random download from wallhaven

--- fetch.py
+++ fetch.py
@@ -1,36 +1,56 @@
+#!/usr/bin/env python3
+
 from os import system
 from time import sleep
 import re, urllib.request, sys
 
 done = False
-while not done:	
-	# Invalid input
-	if len(sys.argv) > 3:
-		print("Enter only the URL of the thread as the first parameter followed by destination directory ie:\n$ python3 4chMediaFetch.py https://boards.4chan.org/ic/thread/0123456789 \"~/Downloads/images/\"")
-	# Check website, currently only 4chan
-	elif "boards.4chan" not in sys.argv[1]:
-		print("Enter a 4chan/4channel thread URL only. I'm working on compatibility with other websites.")
-		done = True
-	else:
+while not done:
+	sys.stdout.write("1: Random wallpapers\n2: 4channel URL\n")
+	selection = int(input(">>> "))
+	
+	# Random from Wallhaven
+	if selection == 1:
 		try:
-			req = urllib.request.Request(sys.argv[1], headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36"})
+			url = "https://wallhaven.cc/search?categories=111&purity=110&atleast=1920x1080&sorting=random&order=desc"
+			req = urllib.request.Request(url, headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36"})
 			html = urllib.request.urlopen(req)
 		except Exception as e:
 			print(f"Nope. {e}")
 			done = True
 			break
-
-		dictFromResult = dict.fromkeys(re.findall(r"(i\.4cdn\.org\/[a-z]{1,4}\/[0-9]{1,20}\.[a-z]{2,4})", html.read().decode()))
-		listOfImages = list(dictFromResult)
-		threadNumber = sys.argv[1].split("/")[-1].split("#")[0]
-		for image in listOfImages:
-			if len(sys.argv) == 3:
-				# Download to foldedr with specified name.
-				system(f"wget -q https://{image} -P {sys.argv[2]}")
-				print(f"Downloading {image} to {sys.argv[2]}...", end="\r")
-			else:
-				# Download to folder named with thread number.
-				system("wget -q https://{image} -P ~/Downloads/{sys.argv[1].split('#')[0].split('/')[-1]}/")
-				print(f"Downloading {image} to ~/Downloads/{threadNumber}/...", end="\r")
+		
+		# Collect and convert thumbnail id to full image links (data-wallpaper-id="abc123" -> w.wallhaven.cc/full/ab/wallhaven-abc123.jpg)
+		dictFromResult = dict.fromkeys(re.findall(r'data-wallpaper-id="([a-zA-Z0-9]{3,10})"', html.read().decode()))
+		listOfId = list(dictFromResult)
+		
+		for ID in listOfId:
+			image = f"https://w.wallhaven.cc/full/{ID[0:2]}/wallhaven-{ID}.jpg"
+			system(f"wget -q {image} -P ~/Downloads/Random/")
+			print(f"Downloading {listOfId.index(ID) + 1} of {len(listOfId)} to ~/Downloads/Random/", end="\r")
 		done = True
-		print(f"{len(listOfImages)} files saved.", end="\r")
+		print(f"\n{len(listOfId)} images saved.")
+				
+	# 4chan
+	elif selection == 2:
+		url = input("Paste full thread URL here\n>>> ")
+		# fetch thread page
+		try:
+			req = urllib.request.Request(url, headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36"})
+			html = urllib.request.urlopen(req)
+		except Exception as e:
+			print(f"Nope. {e}, please enter the full URL\nie. boards.4channel.org/xyz/thread/1234567")
+			done = True
+			break
+		
+		# Get list of file (png, jpg, webm, gif, whatever) links from "href" in HTML (without "s")
+		dictFromResult = dict.fromkeys(re.findall(r"(i\.4cdn\.org\/[a-z]{1,4}\/[0-9]{1,20}\.[a-z]{2,4})", html.read().decode()))
+		listOfFiles = list(dictFromResult)
+		threadNumber = re.findall("thread/([0-9]{2,22}).", url)[0]
+		
+		for file in listOfFiles:
+			# Download to folder named with thread number.
+			system(f"wget -q https://{file} -P ~/Downloads/{threadNumber}/")
+			print(f"Downloading {file.split('/')[-1]} to ~/Downloads/{threadNumber}/", end="\r")
+		done = True
+		print(f"\n{len(listOfFiles)} files saved.")
